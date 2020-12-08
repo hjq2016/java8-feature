d8396aaffc02c61c8a2f44f23e25a25215ddb3ab
austin <924946361@qq.com>
2020-12-08 18:41:36 +0800

    xin

--- demo/src/main/java/com/hjq/java8/feature/component/LambdaTest.java
+++ demo/src/main/java/com/hjq/java8/feature/component/LambdaTest.java
@@ -4,8 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.*;
 
 /**
  * @Description
@@ -17,18 +16,83 @@ import java.util.function.Function;
 public class LambdaTest {
 
     public void action() {
-        int sum = 0;
-        int a = 2;
-        int b = 3;
-        BiFunction<Integer, Integer, Integer> integerSum = ( v,  u) -> (v +u);
-        sum = integerSum.apply(a, b);
-        log.info("" + sum);
+        String tt = "dsds";
+        log.info("" + tt.startsWith("d"));
+        IntegerSum<String, String> gg = (e)->{
+            log.info(tt);
+            return e;
+        };
+//        tt = "sadd";
+        log.info("" + gg.ss("ds"));
+
+        IntegerSum<String, Person> kk = Person::new;
+        Person ff = kk.ss("sfs");
+        log.info("" + ff.getClass());
+        kk.gg();
+
+        Formula formula = (a -> (a));
+        log.info("" + formula.calculate(12));
+
+        Predicate<Integer> predicate = a -> (a > 0);
+        log.info("" + predicate.test(10));
+        log.info("" + predicate.test(-2));
+        log.info("" + predicate.negate().test(-2));
+
+
+        Function<String, Integer> toI = (a) -> 2;
+        Function<String, String> to2 = toI.andThen(String::valueOf);
+        log.info(to2.apply("123"));
+
+        Supplier<Person> personSupplier = Person::new;
+        personSupplier.get();
+
+        Consumer<Integer> consumer = (e) -> {
+            log.info("{}", e);
+        };
+        Consumer<Integer> consumer1 = (e) -> {
+            log.info("{}", e + 10);
+        };
+        Consumer<Integer> consumer2 = consumer.andThen(consumer1);
+        consumer2.accept(100);
+
+
+
+
 
     }
 }
 @FunctionalInterface
-interface IntegerSum {
-    int ss(int a, int b);
+interface IntegerSum<A, R> {
+    R ss(A t);
+
+    default void gg() {
+        System.out.println("I am default");
+    }
+
 
 //    int kk();
+}
+
+interface Formula {
+    double calculate(int a);
+
+    default double sqrt(int a) {
+        return Math.sqrt(a);
+    }
+}
+
+class Person {
+    private String name1;
+    private String name2;
+    Person() {
+
+    }
+    Person(String firstName) {
+        name1 = firstName;
+    }
+
+    Person(String firstName, String lastName) {
+        name1 = firstName;
+        name2 = lastName;
+    }
 }
--- demo/src/main/java/com/hjq/java8/feature/run/FeatureTestRunner.java
+++ demo/src/main/java/com/hjq/java8/feature/run/FeatureTestRunner.java
@@ -1,11 +1,12 @@
 package com.hjq.java8.feature.run;
 
-import com.hjq.java8.feature.component.DefaultMethodTest;
-import com.hjq.java8.feature.component.LambdaTest;
+import com.hjq.java8.feature.component.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * @Description
@@ -22,9 +23,24 @@ public class FeatureTestRunner implements CommandLineRunner {
     @Autowired
     private LambdaTest lambdaTest;
 
+    @Autowired
+    private OptionalTest optionalTest;
+    @Autowired
+    private StreamTest streamTest;
+    @Autowired
+    private MapTest mapTest;
+    @Autowired
+    private DateApiTest dateApiTest;
+
     @Override
     public void run(String... args) throws Exception {
 //        defaultMethodTest.action();
-        lambdaTest.action();
+//        lambdaTest.action();
+//        optionalTest.action();
+//        streamTest.action();
+//        streamTest.action1();
+//        mapTest.action();
+        dateApiTest.action();
+
     }
 }
